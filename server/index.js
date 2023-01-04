f3aa59e7aabbfb3c78a08cc0246f3eb27660ec02
leomarker <85751407+leomarker@users.noreply.github.com>
2023-01-04 20:13:58 +0300

    integrating socket.io

--- server/index.js
+++ server/index.js
@@ -2,20 +2,37 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const mongoConnect = require("./util/db").mongoConnect;
 const mongoose = require("mongoose");
+const { Server } = require("socket.io");
+const http = require("http");
+const cors = require("cors");
 
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+
 const PORT = "5000";
+const server = http.createServer(app);
 
 app.use(express.json({ extended: false }));
+app.use(cors());
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
 
 app.use(authRoutes);
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/melktega")
   .then(() => {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log("server runing at port 5000");
     });
   })
