715b3e3afe43490161696f82bd87ac391e367c00
leomarker <85751407+leomarker@users.noreply.github.com>
2023-01-04 09:35:37 +0300

    new schema

---
+++ server/model/userProfile.js
@@ -0,0 +1,14 @@
+const Mongoose = require("mongoose");
+
+const { Schema } = Mongoose;
+
+const userProfileSchema = new Schema(
+  {
+    name: String,
+    userName: String,
+    profilePicturePath: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = Mongoose.model("UserProfile", userSchema);
