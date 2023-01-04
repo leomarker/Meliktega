5d257f17cd2740dac9f3b29954e3d95d0fd96639
leomarker <85751407+leomarker@users.noreply.github.com>
2023-01-04 09:41:51 +0300

    mongoose schema

---
+++ server/model/messages.js
@@ -0,0 +1,14 @@
+const Mongoose = require("mongoose");
+
+const { Schema } = Mongoose;
+
+const messages = new Schema(
+  {
+    fromUserName: Mongoose.ObjectId,
+    toUserName: Mongoose.ObjectId,
+    messagesTest: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = Mongoose.model("UserProfile", userSchema);
