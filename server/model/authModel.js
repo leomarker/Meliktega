39dce18262f607b53e2386a0433f06308c54c5b7
leomarker <85751407+leomarker@users.noreply.github.com>
2023-01-04 09:22:51 +0300

    #

--- server/model/authModel.js
+++ server/model/authModel.js
@@ -2,9 +2,12 @@ const Mongoose = require("mongoose");
 
 const { Schema } = Mongoose;
 
-const userSchema = new Schema({
-  email: String,
-  password: String,
-});
+const userSchema = new Schema(
+  {
+    email: String,
+    password: String,
+  },
+  { timestamps: true }
+);
 
 module.exports = Mongoose.model("User", userSchema);
