c7c2a5550fa0af51662c23cc016720166db15fb7
leomarker <85751407+leomarker@users.noreply.github.com>
2023-01-05 09:22:28 +0300

    api end point for setProfile

--- client/src/componets/SetProfile.jsx
+++ client/src/componets/SetProfile.jsx
@@ -5,23 +5,32 @@ const SetProfile = () => {
   const [name, setName] = useState(null);
   const [userName, setUsername] = useState(null);
 
+  const handelSubmit = () => {};
+
   return (
     <div className="flex justify-center items-center h-[100vh]">
       <div className="h-[600px] w-[800px] flex justify-center items-center bg-black rounded-2xl flex-col">
         <div className="bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full h-[100px] w-[100px] mb-[20px]"></div>
         <div className="flex flex-col">
-          <input
-            type="text"
-            placeholder="Name"
-            className="w-[400px] h-[40px] mb-[20px] rounded-xl p-[10px]"
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="@username"
-            className="w-[400px] h-[40px] mb-[20px] rounded-xl p-[10px]"
-            onChange={(e) => setUsername(e.target.value)}
-          />
+          <form
+            action=""
+            onSubmit={handelSubmit}
+            className="flex justify-center items-center flex-col mb-[30px]"
+          >
+            {" "}
+            <input
+              type="text"
+              placeholder="Name"
+              className="w-[400px] h-[40px] mb-[20px] rounded-xl p-[10px]"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="@username"
+              className="w-[400px] h-[40px] mb-[20px] rounded-xl p-[10px]"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </form>
         </div>
         <div>
           <button className=" bg-gradient-to-r from-cyan-500 to-blue-500">
