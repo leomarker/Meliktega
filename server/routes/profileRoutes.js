44929dc4572730695a9006203bd5c2a6437f4e50
leomarker <85751407+leomarker@users.noreply.github.com>
2023-01-06 17:19:57 +0300

    adding backend validator

--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
+const { body: bodyValidator } = require("express-validator");
 
 const setProfile = require("../controller/profileController").setProfile;
+
+const validatSignUp = [
+  bodyValidator("name").notEmpty().withMessage("Enter a name"),
+  bodyValidator("userName").notEmpty().withMessage("Enter a userName"),
+];
 
 router.post(
   "/api/setProfile",
