f0ed1cd37991ee7f843b1bf319c4fdce3704ef1c
leomarker <natnael3370@gmail.com>
2023-01-18 19:01:04 +0300

    fixing private route

--- client/src/componets/PrivateRoutes.jsx
+++ client/src/componets/PrivateRoutes.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
 const PrivateRoutes = () => {
-  let auth = localStorage.getItem("auth");
-  console.log(auth);
+  const auth = useAuth();
 
-  if (auth) {
+  if (auth.access) {
     return <Outlet />;
   } else {
     return <Navigate to="/login" />;
