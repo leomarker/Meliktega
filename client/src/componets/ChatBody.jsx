268c043ee51581c9bdcc969e7b61a311208b1864
Natnael Assefa <natnael3370@gmail.com>
2023-02-02 11:20:38 +0300

    dynamic chat body

--- client/src/componets/ChatBody.jsx
+++ client/src/componets/ChatBody.jsx
@@ -8,33 +8,35 @@ const ChatBody = ({ messageResponse }) => {
     <div>
       <div className="message">
         {messageResponse.map((message) => {
-          message.username === user.userName ? (
-            <div className="message__outer">
-              <div className="message__inner">
-                <div className="message__avatar flex justify-center items-center">
-                  <p>A</p>
-                </div>
-                <div className="message__bubble ml-[20px] mr-[10px] mb-[20px] p-[15px]">
-                  {message.text}
-                </div>
-              </div>
-
-              <div></div>
-            </div>
-          ) : (
-            <div className="message__incoming">
-              <div className="message__incoming__inner">
-                <div className="message__incoming__avatar flex justify-center items-center">
-                  <p>A</p>
-                </div>
-                <div className="message__incoming__bubble ml-[20px] mr-[10px] mb-[20px] p-[15px]">
-                  {message.text}
+          if (message.username === user.userName) {
+            return (
+              <div className="message__outer">
+                <div className="message__inner">
+                  <div className="message__avatar flex justify-center items-center">
+                    <p>A</p>
+                  </div>
+                  <div className="message__bubble ml-[20px] mr-[10px] mb-[20px] p-[15px]">
+                    {message.text}
+                  </div>
                 </div>
               </div>
+            );
+          } else {
+            return (
+              <div className="message__incoming">
+                <div className="message__incoming__inner">
+                  <div className="message__incoming__avatar flex justify-center items-center">
+                    <p>A</p>
+                  </div>
+                  <div className="message__incoming__bubble ml-[20px] mr-[10px] mb-[20px] p-[15px]">
+                    {message.text}
+                  </div>
+                </div>
 
-              <div></div>
-            </div>
-          );
+                <div></div>
+              </div>
+            );
+          }
         })}
 
         {/* <div className="message__outer">
